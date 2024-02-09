37b87c228ed8a601eef9c30ba175a43428f508a6
Jose Luis <joseluistresserras@gmail.com>
2024-02-09 17:28:13 +0100

    Move inside snipet

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,7 +1,12 @@
 return {
-	"L3MON4D3/LuaSnip",
-	-- follow latest release.
-	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	-- install jsregexp (optional!).
-	build = "make install_jsregexp"
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    config = function()
+        local luasnip = require('luasnip')
+        vim.keymap.set({ "i", "s" }, "<C-l>", function() luasnip.jump(1) end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<C-h>", function() luasnip.jump(-1) end, { silent = true })
+    end
 }
