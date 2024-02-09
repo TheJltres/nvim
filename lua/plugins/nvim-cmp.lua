87cbc81462c0d2708a75858c721f65065fcc12a1
Jose Luis <joseluistresserras@gmail.com>
2024-02-09 17:28:30 +0100

    Improve cmp motions

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,3 +1,9 @@
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 return {
     "hrsh7th/nvim-cmp",
     name = "nvim-cmp",
@@ -32,32 +38,47 @@ return {
                     end
                 end,
                 ['<C-e>'] = cmp.mapping.abort(),
-                ['.'] = cmp.mapping.confirm({
-                    behaior = cmp.ConfirmBehavior.Replace,
-                    select = false -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<CR>"] = cmp.mapping({
+                    i = function(fallback)
+                        if cmp.visible() then
+                            cmp.confirm({ behavior = select_opts.behavior, select = true })
+                        else
+                            fallback()
+                        end
+                    end,
+                    s = cmp.mapping.confirm({ select = true }),
+                    c = cmp.mapping.confirm({ behavior = select_opts.behavior, select = true }),
                 }),
-                ['<CR>'] = cmp.mapping.confirm({
-                    behaior = cmp.ConfirmBehavior.Replace,
-                    select = false -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                }),
-                ['<Tab>'] = cmp.mapping(function(fallback)
-                    local col = vim.fn.col('.') - 1
-
+                ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
-                        cmp.select_next_item(select_opts)
-                    elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-                        fallback()
-                    else
+                        if #cmp.get_entries() == 1 then
+                            cmp.confirm({ select = true })
+                        else
+                            cmp.select_next_item(select_opts)
+                        end
+                    elseif luasnip.expand_or_jumpable() then
+                            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                            -- that way you will only jump inside the snippet region
+                        luasnip.expand_or_jump()
+                    elseif has_words_before() then
                         cmp.complete()
-                    end
-                end, { 'i', 's' }),
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item(select_opts)
+                        if #cmp.get_entries() == 1 then
+                            cmp.confirm({ select = true })
+                        end
                     else
                         fallback()
                     end
-                end, { 'i', 's' }),
+                end, { "i", "s" }),
+
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
             }),
             sources = cmp.config.sources({
                 { name = 'path' },
