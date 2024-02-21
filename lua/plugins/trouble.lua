69cd3912900b96cafaad4bac5c8e68d50a62da63
TheJltres <joseluistresserras@gmail.com>
2024-02-21 20:00:07 +0100

    Better remaps for references

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,11 +2,14 @@ return {
     "folke/trouble.nvim",
     dependencies = {
         "nvim-telescope/telescope.nvim",
+        "nvim-tree/nvim-web-devicons",
     },
     opts = {
         icons = false,
     },
-    config = function ()
-        vim.keymap.set("n", "<leader>tt", function() require("trouble").toggle() end)
+    config = function()
+        local trouble = require('trouble')
+        vim.keymap.set("n", "<leader>tt", function() trouble.toggle("workspace_diagnostics") end)
+        vim.keymap.set("n", "gr", function() trouble.toggle("lsp_references") end)
     end
 }
