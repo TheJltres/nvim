f2ee9deea2dcc869cfa364027bcb7765a3d51521
TheJltres <joseluistresserras@gmail.com>
2024-02-21 19:59:50 +0100

    Improve cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -10,6 +10,9 @@ return {
     dependencies = {
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
     },
     config = function()
         local cmp = require("cmp")
