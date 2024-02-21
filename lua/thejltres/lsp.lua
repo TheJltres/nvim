3aa1e9200350d5792db1c9ce595246f17d79dcc1
TheJltres <joseluistresserras@gmail.com>
2024-02-21 18:27:18 +0100

    Resolve keymap conflict

--- lua/thejltres/lsp.lua
+++ lua/thejltres/lsp.lua
@@ -16,7 +16,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, opts)
         vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>ga', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
         vim.keymap.set('n', '<leader>f', function()
             vim.lsp.buf.format { async = true }
