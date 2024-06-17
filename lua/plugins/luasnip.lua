cf12c9f43ce7d1d7414a483e646ec8ceb14d778e
Jose Luis <joseluistresserras@gmail.com>
2024-06-17 10:13:49 +0200

    Add new cleaner config

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -4,9 +4,10 @@ return {
     version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
     -- install jsregexp (optional!).
     build = "make install_jsregexp",
-    config = function()
-        local luasnip = require('luasnip')
-        vim.keymap.set({ "i", "s" }, "<C-l>", function() luasnip.jump(1) end, { silent = true })
-        vim.keymap.set({ "i", "s" }, "<C-h>", function() luasnip.jump(-1) end, { silent = true })
+    config = function ()
+        local ls = require("luasnip")
+
+        vim.keymap.set({"i", "s"}, "<C-l>", function() ls.jump( 1) end, {silent = true})
+        vim.keymap.set({"i", "s"}, "<C-j>", function() ls.jump(-1) end, {silent = true})
     end
 }
